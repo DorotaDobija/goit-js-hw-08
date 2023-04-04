87671bcd5cff1fe9e62b5a110a1b4056f0373244
DorotaDobija <dorota.dobija93@gmail.com>
2023-04-04 18:16:00 +0200

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+galleryItems.forEach(picture =>
+  galleryList.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__link"> <a class="gallery__item" href="${picture.original}">
+<img class="gallery__image" src="${picture.preview}" title="${picture.description}" />
+</a></li>`
+  )
+);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
