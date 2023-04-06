c54e2a40707c4f18dd64e688bc4260402c2ac820
DorotaDobija <dorota.dobija93@gmail.com>
2023-04-06 18:25:36 +0200

    task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', timeRecord);
+
+function timeRecord() {
+  player.getCurrentTime().then(
+    throttle(seconds => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    }, 1000)
+  );
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then();
