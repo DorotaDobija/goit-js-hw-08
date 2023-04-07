2b5ea531b9bc19231612afc9ec3329a060aa9f27
DorotaDobija <dorota.dobija93@gmail.com>
2023-04-07 16:56:42 +0200

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textEl = document.querySelector('textarea');
+
+const formText = {
+  email: '',
+  message: '',
+};
+
+function saveState(state) {
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}
+
+formEl.addEventListener(
+  'input',
+  throttle(event => {
+    if (event.target.name === 'email') {
+      formText.email = event.target.value;
+      saveState(formText);
+    }
+
+    if (event.target.name === 'message') {
+      formText.message = event.target.value;
+      saveState(formText);
+    }
+  }, 500)
+);
+
+const inputText = JSON.parse(localStorage.getItem('feedback-form-state'));
+inputEl.value = inputText.email;
+textEl.value = inputText.message;
+
+formEl.addEventListener('submit', e => {
+  console.log(inputText);
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  inputEl.value = '';
+  textEl.value = '';
+});
